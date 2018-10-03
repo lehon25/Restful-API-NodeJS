d586570a08619261dcd9a06c0d60402ec7351eb7
lehon aritonang <lehon_25@yahoo.com>
2018-10-03 21:26:29 +0700

    Restful-API

---
+++ README.md
@@ -0,0 +1,13 @@
+# This is About RESTFUL API NODE JS
+## feature
+1. Express
+2. MongoDB
+3. Asyncronus Javascript
+4. CRUD
+5. Mongoose Validation
+6. Mongoose Modeling
+7. Authenticate and Authorize
+8. Handling and Logging Error
+9. Unit Testing
+10. Test Drivent Development
+11. Deployment  
---
+++ startup/logging.js
@@ -0,0 +1,14 @@
+const winston = require('winston')
+require('winston-mongodb')
+require('express-async-errors')
+
+module.exports = function(){
+    winston.handleExceptions(
+new winston.transports.Console({ colorize: true, prettyPrint: true}),
+new winston.transports.File({ filename:'uncaughtExceptions.log'}))
+process.on('unhandleRejection',(ex)=>{
+  throw ex  
+})
+winston.add(new winston.transports.File({filename:'logfile.log'}))
+winston.add(new winston.transports.MongoDB({db:'mongodb://localhost/vidly',level:'info'}))
+}
